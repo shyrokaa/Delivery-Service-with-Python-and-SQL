6a3c3e9182145e5bd586f0938f8434623c479bbf
shyrokaa <jessthenimble@gmail.com>
2022-01-02 20:37:42 +0200

    some stuff

--- main.py
+++ main.py
@@ -119,9 +119,9 @@ class DB():
 # global database for simplicity
 DATABASE = DB()
 
-
 class User():
     def __init__(self):
+
         self.first_name = ""
         self.last_name = ""
         self.email = ""
@@ -130,6 +130,19 @@ class User():
         self.street = ""
         self.number = ""
         self.password = ""
+
+        # grade - to make a distinction between regular user | manager
+
+        # regular user should be able to:
+        # create orders
+        # view his own orders
+
+        # manager should be able to:
+        # create routes for the drivers
+        # remove or mark orders as completed for each user
+        # 
+
+        self.grade = ""
 
     def add(self, fname_in, lname_in, email_in, phone_in, city_in, street_in, number_in, password_in):
         self.first_name = fname_in
